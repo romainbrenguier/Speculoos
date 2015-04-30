9713613498a57fe403e83d8f7dc42a113b71d6d1
Romain Brenguier <romain.brenguier@gmail.com>
2015-04-30 17:48:51 +0200

    changed the way we treat disjunction of regular expressions

--- temporary1.ml
+++
@@ -1,6 +0,0 @@
-open Speculog
-open Expr
-open SpeculogSyntax
-let main = 
-  output cost 2 in
-Speculog.print_aiger (Speculog.add_synthesized _declarations (ite (dx = 0 || dx = 2) (cost = 1) (cost = 0)) (Aiger.read_from_file Sys.argv.(1)))
