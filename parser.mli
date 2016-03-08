f8c0a23271b2b01a74c222a01135895be77f2f3d
Romain Brenguier <romain.brenguier@cs.ox.ac.uk>
2016-03-08 21:15:07 +0000

    parsing speculog file

--- parser.mli
+++ parser.mli
@@ -1,3 +1,5 @@
 
 (* The first list contains the initial condition and the second one the updates *)
 val parse : char Stream.t -> (Expression.t * Expression.t) list * (Expression.t * Expression.t) list
+
+val parse_inch : in_channel -> (Expression.t * Expression.t) list * (Expression.t * Expression.t) list
