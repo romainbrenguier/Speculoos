1f2c20051520a00df541ea85d73253a850874f5f
Romain Brenguier <romain.brenguier@cs.ox.ac.uk>
2016-06-07 15:43:32 +0100

    script to add inputs to aig files

--- examples/addinputs2aig.ml
+++ examples/addinputs2aig.ml
@@ -5,24 +5,20 @@ let parse stream =
     | [< 'Genlex.Ident x >] -> parse_inputs (x :: accu) stream
     | [< >] -> accu
   in
-  stream |> 
-      parser
-  | [< 'Genlex.Kwd "."; 'Genlex.Kwd "inputs" >] -> parse_inputs [] stream
+  stream |> parser
+    | [< 'Genlex.Kwd "."; 'Genlex.Kwd "inputs" >] -> parse_inputs [] stream
 
 let main = 
   if Array.length Sys.argv < 3 
   then Printf.printf "usage: %s <file.part> <file.aig>" Sys.argv.(0)
   else
     let inputs = Sys.argv.(1) |> open_in |> Stream.of_channel
-  |> lexer |> parse 
+    |> lexer |> parse 
     in
     let aig = Aiger.read_from_file Sys.argv.(2) in
-    let old_inputs = List.map (Aiger.lit2string aig) aig.Aiger.inputs in
     let new_aig = List.fold_left 
       (fun a i -> 
-	(*if List.mem i old_inputs then a
-	else*)
-	  let a,v = Aiger.new_var a in
-	  Aiger.add_input a (Aiger.var2lit v) (i,None)) Aiger.empty inputs in
-    Aiger.write (Aiger.compose new_aig aig) stdout
+	let a,v = Aiger.new_var a in
+	Aiger.add_input a (Aiger.var2lit v) (i,None)) Aiger.empty inputs 
+    in Aiger.write (Aiger.compose new_aig aig) stdout
 
