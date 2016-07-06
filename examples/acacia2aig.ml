ddec5ab7dea0d947c789c0bdcd6ed3eb6a18da00
Romain Brenguier <romain.brenguier@cs.ox.ac.uk>
2016-07-06 17:35:37 +0100

    corrected a problem with disjunction in acacia2aig

--- examples/acacia2aig.ml
+++ examples/acacia2aig.ml
@@ -2,12 +2,13 @@
    ocamlbuild -tag camlp4 -tag use_ocaml-cudd -tag use_ocaml-aiger acacia2aig.byte
 *)
 open Speculoos
+open Expression
 
-let lexer = Genlex.make_lexer ["State"; "initial"; ":"; "("; ")"; ","; "!"; "&&"; "U"; "T"; "||"; "{"; "}"; "to"; "state"; "labeled"]
+let lexer = Genlex.make_lexer ["State"; "initial"; ":"; "("; ")"; ","; "!"; "&&"; "U"; "T"; "F"; "||"; "{"; "}"; "to"; "state"; "labeled"]
 
-
-let expr_of_conjunction =
-  List.fold_left (fun e (b,x) -> if b then e $& x else e $& neg x) (Expression.bool true)
+type conjunction = (bool * Expression.t) list
+type dnf_formula = conjunction list
+type state = { id: int; init: bool; trans: (int * (dnf_formula * conjunction) list) list }
 
 let parse = 
   let tab_variables = Hashtbl.create 10 in
@@ -21,9 +22,9 @@ let parse =
   let rec parse_conjunction accu = parser
     | [< 'Genlex.Kwd "!"; e = parse_var_conjunction; f = parse_remainder_conjunction ((false,e)::accu) >] ->  f
     | [< e = parse_var_conjunction; f = parse_remainder_conjunction ((true,e)::accu) >] ->  f
-    | [< 'Genlex.Kwd "("; e = parse_conjunction [] ; 'Genlex.Kwd ")";  f = parse_remainder_conjunction (List.rev_append e accu)>] -> f
   and parse_var_conjunction = parser
       | [< 'Genlex.Kwd "T" >] -> Expression.bool true
+      | [< 'Genlex.Kwd "F" >] -> Expression.bool false
       | [< 'Genlex.Ident v >] ->  
 	try Hashtbl.find tab_variables v 
 	with Not_found -> 
@@ -32,13 +33,19 @@ let parse =
 	  var
   and parse_remainder_conjunction accu = parser
       | [< 'Genlex.Kwd "&&"; e = parse_conjunction accu >] -> e
-      | [< 'Genlex.Kwd "||"; e = parse_conjunction [] >] -> [true, Expression.disj (expr_of_conjunction accu) (expr_of_conjunction e)]
+      | [< >] -> accu
+  in
+
+  let rec parse_dnf accu = parser
+   | [< 'Genlex.Kwd "("; e = parse_conjunction []; 'Genlex.Kwd ")"; f = parse_remainder_dnf (e::accu) >] -> f
+   | [< e = parse_conjunction []; f = parse_remainder_dnf (e::accu) >] -> f
+  and parse_remainder_dnf accu = parser
+      | [< 'Genlex.Kwd "||"; e = parse_dnf accu >] -> e
       | [< >] -> accu
   in
 
   let parse_implication = parser
-    | [< 'Genlex.Kwd "T"; 'Genlex.Kwd "U"; 'Genlex.Kwd "("; f = parse_conjunction []; 'Genlex.Kwd ")" >] -> ([true,Expression.bool true],f)
-    | [< 'Genlex.Kwd "("; e = parse_conjunction [] ; 'Genlex.Kwd ")"; 'Genlex.Kwd "U"; 'Genlex.Kwd "("; f = parse_conjunction []; 'Genlex.Kwd ")" >] -> (e,f)
+    | [< 'Genlex.Kwd "("; e = parse_dnf [] ; 'Genlex.Kwd ")"; 'Genlex.Kwd "U"; 'Genlex.Kwd "("; f = parse_conjunction []; 'Genlex.Kwd ")" >] -> (e,f)
   in
 
   let rec parse_label accu = parser
@@ -55,7 +62,7 @@ let parse =
   in
 
   let parse_state = parser
-    | [< 'Genlex.Int s; i = is_initial; 'Genlex.Kwd ":"; 'Genlex.Ident outgoing; 'Genlex.Ident transitions; 'Genlex.Kwd ":"; list = parse_transitions [] >] -> s,i,list
+    | [< 'Genlex.Int s; i = is_initial; 'Genlex.Kwd ":"; 'Genlex.Ident outgoing; 'Genlex.Ident transitions; 'Genlex.Kwd ":"; list = parse_transitions [] >] -> {id=s;init=i;trans=list}
   in
   
   let rec parse_transition_system accu = parser
@@ -66,6 +73,13 @@ let parse =
   parser
   | [< 'Genlex.Ident transition; 'Genlex.Ident system; 'Genlex.Kwd "{"; e=parse_transition_system []; 'Genlex.Kwd "}" >] ->  e
 
+
+let conjunction_to_expr = 
+  List.fold_left (fun e (b,x) -> if b then e $& x else e $& neg x) (Expression.bool true) 
+
+let dnf_to_expr = 
+  List.fold_left (fun e c -> e $| conjunction_to_expr c) (Expression.bool false)
+
     
 let to_speculog transition_system = 
   let nb_states = List.length transition_system in
@@ -73,17 +87,18 @@ let to_speculog transition_system =
 
   let output_tab = Hashtbl.create nb_states in
 
-  let state = Expression.var "state" (Type.int (Common.log nb_states)) in
+  let state_var = Expression.var "state" (Type.int (Common.log nb_states)) in
   
   List.iter 
-    (fun (s,i,trans) ->
-      if i && s <> 0 then failwith "initial state is different from 0";
-      if s >= nb_states then failwith "index of state is greater than the number of states";
+    (fun state ->
+      if state.init && state.id <> 0 then failwith "initial state is different from 0";
+      if state.id >= nb_states then failwith "index of state is greater than the number of states";
       List.iter
 	(fun (target,update_list) ->
 	  List.iter (fun (inputs,outputs) ->
-	    let expr = expr_of_conjunction inputs in
-	    let s_and_expr = expr $& (state $= Expression.int s) in
+	    (*let expr = List.fold_left (fun e (b,x) -> if b then e $& x else e $& neg x) (Expression.bool true) inputs in*)
+	    let expr = dnf_to_expr inputs in
+	    let s_and_expr = expr $& (state_var $= Expression.int state.id) in
 	    List.iter (fun (pos,out) ->
 	      if pos 
 	      then 
@@ -98,16 +113,15 @@ let to_speculog transition_system =
 		(p $| s_and_expr)
 	    with Not_found -> Hashtbl.add tab target s_and_expr
 	  ) update_list
-	) trans
+	) state.trans
     ) transition_system;
-
 
   let update_state =
     Hashtbl.fold (fun k e accu -> 
       Expression.ite e (Expression.int k) accu) tab (Expression.int 0)
   in
   
-  Hashtbl.fold (fun k e accu -> add_update accu k e) output_tab  (Update (state, update_state))
+  Hashtbl.fold (fun k e accu -> add_update accu k e) output_tab  (Update (state_var, update_state))
     
       
 
@@ -118,7 +132,7 @@ let main =
     let spec = parse stream in 
     close_in inch;
     Cudd.init 30;
-    spec |> to_speculog |> compile;
+    spec |> to_speculog |> compile ;
     Cudd.quit ()
   with Stream.Error _ -> Printf.eprintf "%s\n" (Parser.remaining_tokens stream)
 
