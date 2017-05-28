ae442bb24e3ba91f270a4e0f7930304cd291c09a
Romain Brenguier <romain.brenguier@gmail.com>
2017-05-28 22:44:23 +0100

    Interface for key.mli

--- diagram/key.mli
+++ diagram/key.mli
@@ -1,8 +1,9 @@
-(* String like object with constant time comparison *)
+(* String-like object with constant time comparison *)
 
 class key :
   string ->
 object
+  method get_id : int
   method compare : key -> int
   method equals_string : string -> bool
   method to_string : string
