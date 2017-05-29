3e00d9f96ac1c9103d1cf0e27ebfa5d72e850af9
Romain Brenguier <romain.brenguier@gmail.com>
2017-05-29 13:36:58 +0100

    More methods in vector

--- diagram/vector.ml
+++ diagram/vector.ml
@@ -1,5 +1,5 @@
-(* 
-   Module: Vector. Implement a class vector with constant amortized time 
+(*
+   Module: Vector. Implement a class vector with constant amortized time
            push_back, push_front, pop_back, pop_front and access at arbitrary
            index.
 
@@ -22,12 +22,15 @@ object(self)
     else
       diff
 
+  method empty =
+    front_pos = back_pos + 1
+
   method front =
     array.(front_pos)
 
   method back =
     array.(back_pos)
-      
+
   method get i =
     array.((front_pos + i) mod capacity)
 
@@ -43,7 +46,7 @@ object(self)
     back_pos <- self#size - 1;
     front_pos <- 0;
     array <- arr;
-    capacity <- size      
+    capacity <- size
 
   method push_front elt =
     front_pos <- (capacity + front_pos - 1) mod capacity;
@@ -52,7 +55,7 @@ object(self)
     if front_pos = (back_pos + 2) mod capacity
     then
       self#resize (capacity * 2)
-	
+
   method push_back elt =
     back_pos <- (back_pos + 1) mod capacity;
     array.(back_pos) <- elt;
@@ -76,11 +79,62 @@ object(self)
     do
       let () = f (self#get i) in ()
     done;
-    
+
   method iteri f =
     for i = 0 to self#size - 1
     do
       let () = f i (self#get i) in ()
     done;
 
-end 
+  method to_string element_to_string separator =
+    let buf = Buffer.create 64 in
+    if self#size > 0
+    then
+      Buffer.add_string buf (element_to_string (self#get 0));
+    for i = 1 to self#size - 1
+    do
+      Buffer.add_string buf separator;
+      Buffer.add_string buf (element_to_string (self#get i));
+    done;
+    Buffer.contents buf
+
+  method append (arr:'a vector) =
+    (* Do at most one resize operation *)
+    if arr#size + self#size >= capacity
+    then
+      self#resize (2 * (arr#size + self#size));
+
+    for i = 0 to arr#size - 1
+    do
+      self#push_back (arr#get i)
+    done;
+
+  method reduce f =
+    if self#empty
+    then
+      raise (Invalid_argument "empty vector");
+    
+    let res = ref (self#get 0) in
+    for i = 1 to self#size - 1
+    do
+      res := f !res (self#get i)
+    done;
+    !res
+
+end
+
+let map f (vec:'a vector) =
+  let v = new vector (f vec#front) in
+  for i = 0 to vec#size - 1
+  do
+    v#push_back (f (vec#get i))
+  done;
+  v
+
+let map_to_list f (vec:'a vector) =
+  let l = ref [] in
+  for i = 0 to vec#size - 1
+  do
+    l := f (vec#get i) :: !l
+  done;
+  !l
--- diagram/vector.mli
+++ diagram/vector.mli
@@ -26,6 +26,9 @@ object
   (* Add an element at the back *)
   method push_back : 'a -> unit
 
+  (* Append a vector at the back *)
+  method append : 'a vector -> unit
+
   (* Set the value of the element at the given index *)
   method set : int -> 'a -> unit
     
@@ -34,5 +37,21 @@ object
 
   (* Applies a function to all index and elements in order *)
   method iteri : (int -> 'a -> unit) -> unit
+
+  (* Converts to string using the given function and adding separators *)
+  method to_string : ('a -> string) -> string -> string
+
+  (* Tells whether the vector is empty *)
+  method empty : bool
+
+  (* Applies a function to elements in succession: f (f (f a1 a2) a3) a4 ... *)
+  method reduce : ('a -> 'a -> 'a) -> 'a
     
 end
+
+(* Applies a function to each element *)
+val map : ('a -> 'b) -> 'a vector -> 'b vector
+    
+(* Converts to a list applying the given function to each element *)
+val map_to_list : ('a -> 'b) -> 'a vector -> 'b list
+
